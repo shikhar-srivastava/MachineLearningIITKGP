6e3a8babdd5ff8c4e45a79bce0ec20066b1e68bc
Shikhar Srivastava <shikharsrivastav@gmail.com>
2018-03-26 16:21:38 +0530

    Installation batch file created

--- install.bat
+++ install.bat
@@ -5,10 +5,17 @@ set NLM=^
 
 
 set NL=^^^%NLM%%NLM%^%NLM%%NLM%
-echo %NL%%NL%%NL%%NL% 1. Download the Dl4j-bin.jar dependency from the link.%NL%%NL% 2. Place the downloaded .jar file in the /jars folder.
-echo %NL%%NL% File Size is ~ 120 MB.
+echo %NL%%NL%%NL%%NL% 1. Download the dependencies from the links.%NL%%NL% 2. Extract the .jar files from the jars.rar into '/jars' folder. %NL%%NL% 3. Similarly, extract lib.rar and bin.rar into 'WEB-INF/lib' and 'WEB-INF/bin'.
+echo.
+echo %NL%%NL% ~~~~ Download .jar dependencies from the link after pressing enter. ~~~~
 echo.
 pause
-start "" "https://yadi.sk/d/j_KO5xP93TkLyq"
+start "" "https://yadi.sk/d/wpvQ38e-3Tm3do"
+echo %NL%%NL% ~~~~ Download and extract bin.rar from the link into WEB-INF/bin. ~~~~
 echo.
 pause
+start "" "https://yadi.sk/d/OyBEcwSe3Tm3oj"
+echo %NL%%NL% ~~~~ Download and extract lib.rar from the link into WEB-INF/lib. ~~~~
+echo.
+pause
+start "" "https://yadi.sk/d/xQHxZV_U3Tm3rm"
